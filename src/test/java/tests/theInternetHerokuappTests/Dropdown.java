dccb4d5ca8c470e8b69f5d66c5c37d096e687996
Anna Kasyan <arelyeve2@gmail.com>
2021-11-04 19:32:06 +0300

    fixed

---
+++ src/test/java/tests/theInternetHerokuappTests/Dropdown.java
@@ -0,0 +1,26 @@
+package tests.theInternetHerokuappTests;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import tests.BaseTest;
+
+import java.util.List;
+
+public class Dropdown extends BaseTest {
+
+    @Test
+    public void dropdownTest() {
+        driver.get("http://the-internet.herokuapp.com/dropdown");
+        Select select = new Select(driver.findElement(By.id("dropdown")));
+        List<WebElement> options = select.getOptions();
+        Assert.assertEquals(options.size(), 3);
+        Assert.assertTrue(options.get(0).isSelected(), "Please select an option - on display");
+        options.get(1).click();
+        Assert.assertTrue(options.get(1).isSelected(), "Option 1 is selected");
+        options.get(2).click();
+        Assert.assertTrue(options.get(2).isSelected(), "Option 1 is selected");
+    }
+}
